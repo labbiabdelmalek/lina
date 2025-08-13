37e171f47cd36a96b539b00f2cb014fe31b9d73d
labbiabdelmalek <l.abdelmalek@casanet.ma>
2025-08-13 10:49:46 +0100

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,15 @@
 // frontend/src/api.js
+import axios from "axios";
+
 export const API_URL =
-  process.env.NODE_ENV === 'production'
-    ? 'https://lina-beg1.onrender.com'
-    : 'http://localhost:5000';
+  process.env.NODE_ENV === "production"
+    ? "https://lina-beg1.onrender.com" // backend على Render
+    : "http://localhost:5000";         // backend local
+
+const api = axios.create({
+  baseURL: API_URL,
+  withCredentials: true, // باش الكوكيز ديال JWT تمشي وتجي
+  headers: { "Content-Type": "application/json" }
+});
+
+export default api;

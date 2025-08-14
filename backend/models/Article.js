aa75dfe7af16b900ac9265bb61b58a7c668c97ad
labbiabdelmalek <l.abdelmalek@casanet.ma>
2025-08-14 23:09:04 +0100

    Update Article.js

--- backend/models/Article.js
+++ backend/models/Article.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const articleSchema = new mongoose.Schema({
-  titre: { type: String, required: true },
-  contenu: { type: String, required: true },
-  image: { type: String },
-  date: { type: Date, default: Date.now }
+  titre:   { type: String, required: true, trim: true },
+  contenu: { type: String, required: true, trim: true },
+  image:   { type: String, default: null },
+  date:    { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Article', articleSchema);

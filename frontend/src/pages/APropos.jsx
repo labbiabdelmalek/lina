43945c6a1818360c807d9648b003c20608d6a591
labbiabdelmalek <l.abdelmalek@casanet.ma>
2025-08-15 10:00:22 +0100

    Create APropos.jsx

---
+++ frontend/src/pages/APropos.jsx
@@ -0,0 +1,13 @@
+function APropos() {
+  return (
+    <div className="container py-5">
+      <h1 className="mb-4">À propos</h1>
+      <p>
+        Bienvenue sur <strong>Lina Blog</strong> — un petit blog pour publier des
+        articles, partager des nouvelles et tester des idées. 
+      </p>
+     
+    </div>
+  );
+}
+export default APropos;

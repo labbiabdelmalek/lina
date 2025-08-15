a3d164bcc90c82a83a59d5ce18247f4f3c20b581
labbiabdelmalek <l.abdelmalek@casanet.ma>
2025-08-15 09:58:40 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Accueil from "./pages/Accueil";
 import Article from "./pages/Article";
 import Login from "./admin/Login";
 import Dashboard from "./admin/Dashboard";
+import APropos from "./pages/APropos";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ProtectedRoute from "./components/ProtectedRoute";
@@ -11,16 +12,22 @@ function App() {
   return (
     <BrowserRouter>
       <Header />
-      <main style={{ padding: '20px' }}>
+      <main style={{ padding: "20px" }}>
         <Routes>
           <Route path="/" element={<Accueil />} />
+          <Route path="/apropos" element={<APropos />} />
           <Route path="/article/:id" element={<Article />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/admin" element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          } />
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          {/* 404 بسيطة */}
+          <Route path="*" element={<div className="container py-5">Page introuvable.</div>} />
         </Routes>
       </main>
       <Footer />

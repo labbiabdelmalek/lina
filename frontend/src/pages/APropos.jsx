4115bec59a2692fa2609a097463d14537b9a459b
labbiabdelmalek <l.abdelmalek@casanet.ma>
2025-08-15 10:04:48 +0100

    Update APropos.jsx

--- frontend/src/pages/APropos.jsx
+++ frontend/src/pages/APropos.jsx
@@ -2,10 +2,13 @@ function APropos() {
   return (
     <div className="container py-5">
       <h1 className="mb-4">À propos</h1>
-      <p>
-        Bienvenue sur <strong>Lina Blog</strong> — un petit blog pour publier des
-        articles, partager des nouvelles et tester des idées. 
-      </p>
+
+      <p>Bonjour et bienvenue dans mon monde coloré !
+J’ai presque 9 ans et je suis passionnée par le dessin.
+Chaque jour, j’aime inventer, créer et donner vie à mes idées avec des crayons, des pinceaux ou des couleurs.
+Ici, je partage mes œuvres, mes inspirations et un petit bout de mon imagination…
+Bonne visite ! ✨✨✨</p>
+     
      
     </div>
   );
